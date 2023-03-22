ef0d6f7dbeb027dc7304354560892ba2069cb3f5
Ayushi0516 <ayushigupta0516@gmail.com>
2023-03-22 21:35:52 +0530

    Crud added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -48,7 +48,7 @@ const TodoList = () => {
     return (
         <>
             <div className = "header text-center">
-                <h3>Todo List</h3>
+                <h2>Todo List</h2>
                 <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)} >Create Task</button>
             </div>
             <div className = "task-container">
